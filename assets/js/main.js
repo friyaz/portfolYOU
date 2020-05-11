e131fd9f48a7f33ec9c771e29a019421632cd41a
Faisal Riyaz <faisalriyaz011@gmail.com>
2020-05-11 10:52:02 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 // Static comments
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 $(function() {
     $("#comment-form").submit(function() {
         var form = this;
